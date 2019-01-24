3892f8684e87c0674da32783e667552c21431a3f
thundernorain <46970392+thundernorain@users.noreply.github.com>
2019-01-24 17:25:23 +0400

    Add files via upload

---
+++ Game15/FormGame15.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game15
+{
+    public partial class FormGame15 : Form
+    {
+        Game game;
+
+        public FormGame15()
+        {
+            InitializeComponent();
+
+            game = new Game(4);
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.Shift(position);
+            CellRefresh();
+        }
+        private Button button(int pos)
+        {
+            switch (pos)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+        private void Start()
+        {
+            game.Start();
+            for (int i = 0; i < 100; i++)
+                game.ShiftRandom();
+            CellRefresh();
+        }
+        private void CellRefresh()//refreshing number writing in cells, cell with a 0 is invisible
+        {
+            for (int pos = 0; pos < 16; pos++)
+            {
+                button(pos).Visible = (game.GetNumber(pos) > 0);
+                button(pos).Text = game.GetNumber(pos).ToString();
+            }
+            if (game.CheckForEnd())
+            {
+                MessageBox.Show("Вы победили! Игра начнется заново", "Поздравляем");
+                Start();
+            }
+        }
+
+        private void FormGame15_Load(object sender, EventArgs e)
+        {
+            Start();
+        }
+    }
+}
---
+++ Game15/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game15
+{
+    class Game
+    {
+        int size;//number of cells
+        int[,] cells;//matrix for game cells
+        int emptyX, emptyY;//coordinats of empty cell
+        static Random Rnd = new Random();//need for set random numbers in cells
+
+        public Game(int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 7) size = 7;
+
+            this.size = size;
+            cells = new int[size, size];
+        }
+        public void Start()
+        {
+            emptyX = size - 1;
+            emptyY = size - 1;
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    cells[i, j] = CoordsToPos(i, j) + 1;
+
+            cells[emptyX, emptyY] = 0;
+        }
+        public bool CheckForEnd()//check for end game
+        {
+            if (cells[size - 1, size - 1] != 0) return false;//if last cell is not empty then game not ended
+            for (int pos = 0; pos < size * size - 1; pos++)
+                if (GetNumber(pos) != pos + 1) return false;//if all cells are in chaos then game not ended
+            return true;//if last cell empty and all cells is sorted then game ended
+        }
+        public int GetNumber(int pos)//get number in cell
+        {
+            int x, y;
+            PosToCoords(pos, out x, out y);
+
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+
+            return cells[x, y];
+        }
+        private int CoordsToPos(int x, int y)//get coordinats from position
+        {
+            if (x < 0) x = 0;
+            if (x > size - 1) x = size - 1;
+            if (y < 0) y = 0;
+            if (y > size - 1) y = size - 1;
+
+            return y * size + x;
+        }
+        private void PosToCoords(int pos, out int x, out int y)//get position from coordinats
+        {
+            if (pos < 0) pos = 0;
+            if (pos > size * size - 1) pos = size * size - 1;
+
+            x = pos % size;
+            y = pos / size;
+        }
+        public void Shift(int pos)//shifting coordinats with empty cell
+        {
+            int x, y;
+            PosToCoords(pos, out x, out y);
+
+            if (Math.Abs(emptyX - x) + Math.Abs(emptyY - y) != 1) return;//check for neighbors with empty cell
+
+            cells[emptyX, emptyY] = GetNumber(pos);
+            cells[x, y] = 0;
+            emptyX = x;
+            emptyY = y;
+        }
+        public void ShiftRandom()//randomly sets the numbers in new game
+        {
+            int x = emptyX;
+            int y = emptyY;
+
+            int num = Rnd.Next(0, 4);
+            switch (num)
+            {
+                case 0: x++;break;
+                case 1: x--;break;
+                case 2: y++;break;
+                case 3: y--;break;
+            }
+
+            Shift(CoordsToPos(x,y));
+        }
+    }
+}
